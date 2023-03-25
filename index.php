36382b8d49846e5e558f6993bb71cbfbf587ebca
Damian Terefenko <4253767@teb-gdansk.ad>
2023-03-25 10:44:28 +0100

    dodawanie laczenia plikow

--- index.php
+++ index.php
@@ -12,7 +12,8 @@ require_once
 
 //$GET -zapytanie servera
 //$POST- wyslanie od servera
- 
+
+ include_once('src/view.php');
 include_once('./src/utilits/debug.php');
  
  
@@ -24,42 +25,6 @@ if(!empty($GET[`action`])){
 }
 
 
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-<header>
-    <h1> Moje Notatki<h1>
-</header>
-
-<main>
-    <nav>
-        <ul>
-            <li> <a href="/">Lista notatek</a></li>
-            <li> <a href ="/?action=create">Nowa notatka</a></li>
-        </ul>
-    </nav>
-    <article>
-        <?php if ($action===`create`) :?>
-            <h3>Nowa Notatka</h3>
-        <?php else :?>
-            <h3>Lista notaek</h3>
-        <?php endif; ?>
-    </article>
-
-
-</main>
-
-<footer> Stopka</footer>
-    
-</body>
-</html>
-
+$action = $_GET['action'] ??null;
+$view = new View();
+$view->render($action);
---
+++ templates/pages/create.php
@@ -0,0 +1,4 @@
+<h3>Nowa notatka</h3>
+<div>
+    Tu bedzie formularz Notatki
+</div>
